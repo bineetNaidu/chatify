24a839330800d5b1954b68d0a7e0ceb99c9f64e9
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-06 02:13:07 +0530

    🚀 base app setup

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import http from 'http';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import path from 'path';
+
+dotenv.config();
+const DEV = process.env.NODE_ENV !== 'production';
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(helmet());
+
+app.get('/', (req, res) => {
+  res.json({
+    hello: 'World',
+  });
+});
+
+// Serve static assets if in production
+if (!DEV) {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
+  });
+}
+
+server.listen(process.env.PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('..........Server Listening.........');
+});
