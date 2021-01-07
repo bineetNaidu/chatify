e9aba5f6a2e1fe95caa0fcb54f5ea9200d76585a
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-07 23:29:23 +0530

    🎉 add server model

---
+++ src/models/Server.ts
@@ -0,0 +1,69 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-shadow */
+import mongoose from 'mongoose';
+
+// ? Interfaces/Types
+enum ServerVisibility {
+  Public = 'public',
+  Private = 'private',
+}
+
+interface User {
+  name: string;
+  email: string;
+  googleId: string;
+  status?: string;
+  isAdmin: boolean;
+}
+
+interface ServerDoc extends mongoose.Document {
+  serverName: string;
+  serverAdmin: User;
+  members: User[] | [];
+  channels: [];
+  visibility: ServerVisibility;
+}
+
+interface ServerModel extends mongoose.Model<ServerDoc> {}
+
+// ? Utilities
+const StringAndRequired = {
+  type: String,
+  required: true,
+};
+
+const StringRequiredAndUnique = {
+  type: String,
+  required: true,
+  unique: true,
+};
+
+const ServerSchema = new mongoose.Schema({
+  serverName: StringRequiredAndUnique,
+  serverAdmin: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  channels: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Channel',
+    },
+  ],
+  visibility: {
+    ...StringAndRequired,
+    enum: Object.values(ServerVisibility),
+    default: ServerVisibility.Public,
+  },
+});
+
+const Server = mongoose.model<ServerDoc, ServerModel>('Server', ServerSchema);
+
+export default Server;
