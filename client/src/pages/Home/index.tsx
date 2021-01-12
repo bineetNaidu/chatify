ee7f3e9ca8b93874c8bb0f63198995a4e53aea1d
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-12 22:20:25 +0530

    add class Names and media queries

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -2,9 +2,16 @@ import { FC } from 'react';
 import './home.scss';
 
 const Home: FC = () => {
+  const handleSignWithGoogle = () => {
+    window.open('https://localhost:4242/api/auth/google');
+  };
+
   return (
     <div className="home">
-      <h1>Welcome to Chatify</h1>
+      <h1 className="home__welcome__text">Welcome to Chatify</h1>
+      <button className="home__signupBtn" onClick={handleSignWithGoogle}>
+        Sign up!
+      </button>
     </div>
   );
 };
