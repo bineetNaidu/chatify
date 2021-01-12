263d74864bd1cea442f69a4414c4ed8c460fcfd4
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-13 00:00:15 +0530

    🚀 using context from State

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import { Switch, Redirect, Route } from 'react-router-dom';
 import { io as socketIO } from 'socket.io-client';
+import { useContextStateValue } from './data/StateProvider';
 import Home from './pages/Home';
 import UserOnBoardPage from './pages/User';
 
 const io = socketIO();
 
 function App() {
+  const [state, dispatch] = useContextStateValue();
+  console.log(state);
   return (
     <>
       <Switch>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { StateProvider } from './data/StateProvider';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <StateProvider>
+        <App />
+      </StateProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
