abd8ff2714cfeb1158b8d9cfb834f1c8cef3b101
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-13 21:26:15 +0530

    🎉 add new var && add SidePanel Header

--- client/src/components/SidePanel/index.tsx
+++ client/src/components/SidePanel/index.tsx
@@ -1,10 +1,21 @@
 import { FC } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import './SidePanel.scss';
 
 const SidePanel: FC = () => {
   return (
     <div className="sidePanel">
-      <div className="sidePanel__header">{/* PanelHeader */}</div>
+      <div className="sidePanel__header">
+        <Avatar
+          src="https://avatars0.githubusercontent.com/u/66471461?v=4"
+          alt="Bineet"
+        />
+        <IconButton color="secondary">
+          <MoreVertIcon />
+        </IconButton>
+      </div>
       <div className="sidePanel__chatBars">{/* PanelChatBar */}</div>
     </div>
   );
