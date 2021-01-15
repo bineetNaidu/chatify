c41f454807407e23161628674b4231c5f9c59af8
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-15 22:25:15 +0530

    🐛 fix the Context API

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { Switch, Redirect, Route } from 'react-router-dom';
 import io from './socketio';
-import { useContextStateValue } from './data/StateProvider';
+import { useStateValue } from './data/StateProvider';
 import Home from './pages/Home';
 import UserOnBoardPage from './pages/User';
 import { ActionTypes, User } from './types';
 
 function App() {
-  const [state, dispatch] = useContextStateValue();
+  const [state, dispatch] = useStateValue();
   io.on('USER_ACTIVE', (user: User) => {
     dispatch({
       type: ActionTypes.SetOnline,
@@ -18,7 +18,11 @@ function App() {
     <>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/u" component={UserOnBoardPage} />
+        {state.user ? (
+          <Route exact path="/u" component={UserOnBoardPage} />
+        ) : (
+          <Redirect to="/" />
+        )}
       </Switch>
     </>
   );
--- client/src/data/StateProvider.jsx
+++ client/src/data/StateProvider.jsx
@@ -1,16 +1,15 @@
 import { createContext, useContext, useReducer } from 'react';
 import reducer, { initialState } from './reducer';
 
-//? Prepares the dataLayer
+// Prepares the dataLayer
 export const StateContext = createContext();
 
-//? Wrap our app and provide the Data layer
-export const StateProvider = ({ children }) => {
-  const value = useReducer(reducer, initialState);
-  return (
-    <StateContext.Provider value={value}>{children}</StateContext.Provider>
-  );
-};
+// Wrap our app and provide the Data layer
+export const StateProvider = ({ children }) => (
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </StateContext.Provider>
+);
 
-//? Pull information from the data layer
-export const useContextStateValue = () => useContext(StateContext);
+// Pull information from the data layer
+export const useStateValue = () => useContext(StateContext);
