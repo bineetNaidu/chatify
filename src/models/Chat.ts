d2adb203c4bb197d3f067eb7aa885ba447e174d2
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-16 16:09:36 +0530

    ❇️ add Chat Schema

---
+++ src/models/Chat.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+import mongoose from 'mongoose';
+import { StringAndRequired } from './utils';
+
+// ? Interfaces/Types
+interface ChatDoc extends mongoose.Document {}
+
+interface ChatModel extends mongoose.Model<ChatDoc> {}
+
+const ChatSchema = new mongoose.Schema(
+  {
+    senderId: StringAndRequired,
+    text: StringAndRequired,
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      versionKey: false,
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+    // eslint-disable-next-line comma-dangle
+  }
+);
+
+const Chat = mongoose.model<ChatDoc, ChatModel>('Chat', ChatSchema);
+
+export default Chat;
