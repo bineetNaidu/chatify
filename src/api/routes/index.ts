3eaf89aaafed29bcf0aed6dd2c4956f25177361d
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-16 18:14:15 +0530

    🎉 add create new Room route handler

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import signinOauthRoute from './auth/signinOauth';
 import redirectOauthRoute from './auth/redirectOauth';
+import createRoomRoute from './rooms/create';
 
 const RootAPIRoutes = Router();
 
 // ? Auth Service
 RootAPIRoutes.use('/auth', signinOauthRoute);
 RootAPIRoutes.use('/auth', redirectOauthRoute);
+
+// ? Rooms Service
+RootAPIRoutes.use('/rooms', createRoomRoute);
 
 export default RootAPIRoutes;
