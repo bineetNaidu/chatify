7a7f604294c75eae2c3c43e4d5e44a5ee5a68de9
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-16 15:49:22 +0530

    🎉 updated User Model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,33 +1,72 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
 import mongoose from 'mongoose';
 import { StringAndRequired } from './utils';
 
 // ? Interfaces/Types
 interface UserDoc extends mongoose.Document {
   name: string;
+  avatar?: string;
   googleId: string;
   status?: string;
   isAdmin: boolean;
   online: boolean;
+  blockedLists: string[];
+  friends: {
+    friend: string;
+    room: string;
+  }[];
 }
 
 interface UserModel extends mongoose.Model<UserDoc> {}
 
-const UserSchema = new mongoose.Schema({
-  name: StringAndRequired,
-  googleId: {
-    type: String,
-    unique: true,
+const UserSchema = new mongoose.Schema(
+  {
+    name: StringAndRequired,
+    avatar: String,
+    googleId: {
+      type: String,
+      unique: true,
+    },
+    status: String,
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    online: {
+      type: Boolean,
+      default: false,
+    },
+    blockedLists: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    friends: [
+      {
+        friend: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
+        },
+        room: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Room',
+        },
+      },
+    ],
   },
-  status: String,
-  isAdmin: {
-    type: Boolean,
-    default: false,
-  },
-  online: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    toJSON: {
+      versionKey: false,
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+    // eslint-disable-next-line comma-dangle
+  }
+);
 
 const User = mongoose.model<UserDoc, UserModel>('User', UserSchema);
 
