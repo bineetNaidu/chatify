43bb5fa65f155d70c73a205c664adbe8b3435ba4
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-16 18:30:23 +0530

    🎉 add handleCreateChat SocketIO event handler

--- src/SocketIO/index.ts
+++ src/SocketIO/index.ts
@@ -3,11 +3,18 @@
 import axios from 'axios';
 import { Server } from 'socket.io';
 import handleUserActive from './utils/handleActiveUser';
+import handleCreateChat from './utils/handleCreateChat';
 
 export default function SocketIOServerConnection(io: Server) {
   io.on('connection', (socket) => {
     console.log('>> CONNECTED <<');
 
     handleUserActive(socket);
+
+    handleCreateChat(socket);
+
+    socket.on('ROOM_CREATED', (room: any) => {
+      socket.emit('ROOM_CREATED', room);
+    });
   });
 }
