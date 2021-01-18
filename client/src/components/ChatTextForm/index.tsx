b097453ca96a818cdb83f90a92b33032ef38798a
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-18 22:16:02 +0530

    ❇️ add styles for ChatTextComponent and Socket emitter

--- client/src/components/ChatTextForm/index.tsx
+++ client/src/components/ChatTextForm/index.tsx
@@ -1,13 +1,42 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import Button from '@material-ui/core/Button';
+import { useRoomStateValue } from '../../data/RoomStateProvider';
+import { useUserStateValue } from '../../data/UserStateProvider';
+import io from '../../socketio';
 import './ChatTextForm.scss';
 
 const ChatTextForm: FC = () => {
+  const [text, setText] = useState('');
+  const [{ selectedRoom }] = useRoomStateValue();
+  const [{ user }] = useUserStateValue();
+
+  const handleCreateChat = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    io.emit('CREATE_CHAT', {
+      userId: user.id,
+      roomId: selectedRoom.id,
+      text,
+      senderId: user.id,
+    });
+  };
+
   return (
-    <form className="chatTextForm" onSubmit={(e) => e.preventDefault()}>
-      <input type="text" className="chatTextForm__input" />
-      <button type="submit" className="chatTextForm__btn">
+    <form className="chatTextForm" onSubmit={handleCreateChat}>
+      <input
+        className="chatTextForm__input"
+        placeholder="Message....."
+        autoFocus
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <Button
+        variant="contained"
+        color="secondary"
+        className="chatTextForm__btn"
+        type="submit"
+      >
         Send
-      </button>
+      </Button>
     </form>
   );
 };
