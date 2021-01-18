e85879167a443c8bb5a1423ef536edd6bb1cf54d
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-18 15:21:12 +0530

    🎉 add handleDeleteRoom Socket Event

--- src/SocketIO/index.ts
+++ src/SocketIO/index.ts
@@ -1,9 +1,9 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable no-unused-vars */
-import axios from 'axios';
 import { Server } from 'socket.io';
 import handleUserActive from './utils/handleActiveUser';
 import handleCreateChat from './utils/handleCreateChat';
+import handleDeleteRoom from './utils/handleDeleteRoom';
 import handleGetRooms from './utils/handleGetRooms';
 
 export default function SocketIOServerConnection(io: Server) {
@@ -15,6 +15,8 @@ export default function SocketIOServerConnection(io: Server) {
     handleCreateChat(socket);
 
     handleGetRooms(socket);
+
+    handleDeleteRoom(socket);
 
     socket.on('ROOM_CREATED', (room: any) => {
       socket.emit('ROOM_CREATED', room);
