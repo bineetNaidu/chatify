249c93909a5760089bcc60f610865b099387424d
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-23 15:54:30 +0530

    🚀 add post req handler for Oauth

--- src/api/routes/auth/signinOauth.ts
+++ src/api/routes/auth/signinOauth.ts
@@ -1,14 +1,35 @@
-import { Router } from 'express';
-import passport from 'passport';
+import { Router, Response, Request } from 'express';
+import User from '../../../models/User';
 
 const router = Router();
 
-router.get(
+router.post(
   '/google',
-  passport.authenticate('google', {
-    scope: ['profile'],
+  // eslint-disable-next-line consistent-return
+  async (req: Request, res: Response): Promise<any> => {
+    // eslint-disable-next-line object-curly-newline
+    const { googleId, name, avatar, status, online } = req.body;
+
+    const userExist = await User.findOne({ googleId });
+
+    if (userExist) {
+      return res.status(200).json(userExist);
+    }
+
+    const user = new User({
+      name,
+      avatar,
+      googleId,
+      status,
+      online,
+      blockedLists: [],
+      rooms: [],
+    });
+
+    await user.save();
+    res.status(201).json(user);
     // eslint-disable-next-line comma-dangle
-  })
+  }
 );
 
 export default router;
--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 import signinOauthRoute from './auth/signinOauth';
-import redirectOauthRoute from './auth/redirectOauth';
 import createRoomRoute from './rooms/create';
 
 const RootAPIRoutes = Router();
 
 // ? Auth Service
 RootAPIRoutes.use('/auth', signinOauthRoute);
-RootAPIRoutes.use('/auth', redirectOauthRoute);
 
 // ? Rooms Service
 RootAPIRoutes.use('/rooms', createRoomRoute);
