1447db1663b17ad13e557143b1e8762585ce4de6
bineetNaidu <bineet.s.naidu@gmail.com>
2022-07-21 10:35:35 +0530

    🎉 add react-router-dom

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider resetCSS>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 );
